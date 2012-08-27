8861f25b110f60bcf7ea29c1cee14665cae01892
Thomas Roehr <thomas.roehr@dfki.de>
2012-08-27 15:09:12 +0200

    Generate a test deployment only

--- scripts/start.rb
+++ scripts/start.rb
@@ -1,32 +1,26 @@
-require 'fipa-message'
-include FIPA
-
+require 'readline'
 require 'orocos'
 include Orocos
 
 Orocos.initialize
 
-deployment_name = ENV['FAMOS_SYSTEM_ID']+'_'+'ROOT01'
-mta_name = ENV['FAMOS_SYSTEM_ID']+'_'+'MTA'
+deployment_name = 'test_root'
+prefix = ARGV[0]
 
-Orocos.run deployment_name, "wait" => 100 do |p1|
+Orocos.run deployment_name , :wait => 10, :cmdline_args => { :prefix => prefix } do |p1|
 
 begin
-    root_module = p1.task deployment_name
+    root_module = p1.task "#{prefix}ROOT"
 rescue Orocos::NotFound
     print 'Deployment not found.'
     raise
 end
-    # deprecated: has no effect, use dedicate deployments instead
-    #root_module.module_name = 'A_ROOT_1'
 
-    root_module.avahi_type = '_rimres._tcp'
+    root_module.avahi_type = "_test_root._tcp"
     root_module.avahi_port = 12000
     root_module.avahi_ttl = 0
     root_module.configure
     root_module.start
 
-    while true
-        sleep 1
-    end
+    Readline::readline("Press enter to exit")
 end 
